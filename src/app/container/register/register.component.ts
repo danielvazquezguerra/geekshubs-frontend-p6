0d5ad3d43222b3aefc30dcf984e886fd1cbec9ca
danielvazquezguerra <daniel.vazquezguerra@gmail.com>
2020-04-22 23:09:20 +0200

    login register validated

--- src/app/container/register/register.component.ts
+++ src/app/container/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../../services/users.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -8,7 +9,7 @@ import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  templateUrl: './register.component.html',
+  templateUrl: './angularegister.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
@@ -25,6 +26,7 @@ export class RegisterComponent implements OnInit {
   }
 
   register(registerForm: NgForm){
+    console.log(registerForm);
     const user = registerForm.value;
     this.usuariosService.register(user)
     .subscribe(
@@ -32,7 +34,7 @@ export class RegisterComponent implements OnInit {
         this.successMsg = res[this.successMsg];
         setTimeout(() => {
           this.router.navigate(['login']);
-        }, 2000);
+        }, 5000);
     },
     (error: HttpErrorResponse) => {
       this.errorMsg = error.error.message;
@@ -41,5 +43,20 @@ export class RegisterComponent implements OnInit {
   );
 
   }
+  // signup(signupForm: NgForm) {
+  //   console.log(signupForm);
+  //   if (signupForm.controls.password.errors?.pattern) {
+  //     return this.notification.warning('Wrong password', 'Your password must contain at least a lowercase letter, a uppercase letter, a number, and must be between 8 and 40 characters')
+  //   }
+  //   if (signupForm.valid) {
+  //     this.userService.signup(signupForm.value)
+  //       .subscribe(res => {
+  //         this.notification.success('User created', 'User successfully created', { nzDuration: 2000 });
+  //         setTimeout(() => this.router.navigate(['/login']), 2000);
+  //       });
+  //   }
+  // }
+
+
 // caracter especial (?=.*[!@#$%^&*])
 }
