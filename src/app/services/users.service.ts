85d91bce4bf7b54be1bc54dd0f6e8fa0b38c1721
Ferrowrafael <ferrowrafael@gmail.com>
2020-04-27 17:37:03 +0200

    add model movie

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../models/user.model';
+import { Movie } from '../models/movie.model';
 
 @Injectable({
   providedIn: 'root'
